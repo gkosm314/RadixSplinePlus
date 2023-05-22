522d65577b8009d58d3e1e264c5e5cf448f0ad11
Giorgos Kosmas <gkosm314@gmail.com>
2023-05-22 20:38:34 +0200

    Make constructor save a pointer to the data

--- rsplus.h
+++ rsplus.h
@@ -13,17 +13,21 @@ class RSPlus{
 
  public:
     RSPlus() = delete;                  
-    RSPlus(std::vector<KeyType> * k);
+    RSPlus(std::vector<KeyType> & k);
 
  private:
+    std::vector<KeyType> * keys; //The keys that the active_learned_index approximates.
+
     rs::RadixSpline<KeyType> active_learned_index; //LearnedIndex to which reads are directed
     xindex::AltBtreeBuffer<KeyType, KeyType> * active_delta_index; //DeltaIndex to which writes are directed
-
 };
 
 template <class KeyType>
-RSPlus<KeyType>::RSPlus(std::vector<KeyType> * keys){
+RSPlus<KeyType>::RSPlus(std::vector<KeyType> & k){
     //Assumption: keys is a sorted vector
+
+    //Keys should be
+    keys = &k;
 
     //Extract minimum and maximum value of the data you want to approximate with the spline
     KeyType min_key = keys->front();
@@ -31,11 +35,13 @@ RSPlus<KeyType>::RSPlus(std::vector<KeyType> * keys){
 
     //Construct the spline in a single pass by iterating over the keys
     rs::Builder<KeyType> rsb(min_key, max_key);
-    for (const auto& key : *keys) rsb.AddKey(key); //TODO: check this for performance
+    for (const auto& key : k) rsb.AddKey(key); //TODO: check this for performance
     active_learned_index = rsb.Finalize();
 
     //Create a new empty delta index
     active_delta_index = new xindex::AltBtreeBuffer<KeyType, KeyType>();
 }
+
+
 
 #endif
